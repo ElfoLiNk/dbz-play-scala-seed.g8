047f55b879b5a7ed3459618381813fcd98f07671
Fabio Pirola <fabio.pirola@databiz.it>
2017-09-13 11:39:55 +0200

    Create Custom Base Controller that is a common base for all controllers. This base controller is created in order to define a common functions and extended trait.

--- src/main/g8/$play_module$/src/test/scala/$package$/test/controller/PingControllerSpec.scala
+++ src/main/g8/$play_module$/src/test/scala/$package$/test/controller/PingControllerSpec.scala
@@ -4,14 +4,16 @@ import $package$.controller.PingController
 import org.scalatestplus.play._
 import play.api.test.Helpers._
 import play.api.test._
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import $package$.controller._
 
-class PingControllerSpec extends PlaySpec {
+class PingControllerSpec extends PlaySpec with GuiceOneAppPerTest {
 
   "PingController GET" should {
 
     "return pong string" in {
-      val controller = new PingController
-      val ping       = controller.ping().apply(FakeRequest())
+      val request = FakeRequest(GET, routes.PingController.ping().url)
+      val ping    = route(app, request).get
 
       status(ping) mustBe OK
       contentType(ping) mustBe Some("text/plain")
