b60f8347029f3663326d2716fdc47626b603c5dd
Fabio Pirola <fabio.pirola@databiz.it>
2017-09-15 14:31:39 +0200

    Create simple mathematical controller with a fake product operation

--- src/main/g8/$play_module$/src/main/scala/$package$/controller/PingController.scala
+++ src/main/g8/$play_module$/src/main/scala/$package$/controller/PingController.scala
@@ -4,6 +4,11 @@ import javax.inject._
 import play.api.mvc._
 import play.mvc.Http
 
+
+/**
+ * Ping API that implement a simple pong response that
+ * could be used to verify if an application is UP&Running.
+ */
 @Singleton
 class PingController @Inject()(cc: ControllerComponents) extends CustomBaseController(cc) {
   def ping = Action {
